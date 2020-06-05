7e99c5c4f96a58844cdc3a3cb8baa64a3efc2dc8
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-06-05 09:59:26 +0300

    done

---
+++ app.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('form');
+
+const {hash} = window.location;
+const message = atob(hash.replace('#',''));
+
+if(message){
+	document.querySelector('#message-form').classList.add('hide');
+	document.querySelector('#message-show').classList.remove('hide');
+	document.querySelector('h1').innerHTML= message;
+}
+
+form.addEventListener('submit', (event)=>{
+	event.preventDefault();
+
+	document.querySelector('#message-form').classList.add('hide');
+	document.querySelector('#link-form').classList.remove('hide');
+	const input = form.querySelector('#message-input');
+
+	const encrypted = btoa(input.value);
+
+	const linkInput= document.querySelector('#link-input');
+	linkInput.value = `${window.location}#${encrypted}`;
+	linkInput.select();
+
+})
